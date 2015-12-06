2c1864b848b787e3f78b1f0c937d4f41091f656a
Jack Sneed <sneed.jack@gmail.com>
2015-12-06 00:55:54 -0800

    add initial test

--- jack_sneed/gulpfile.js
+++ jack_sneed/gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 
@@ -16,6 +17,16 @@ gulp.task('webpack:dev', function() {
     }
   }))
   .pipe(gulp.dest('build/'));
+});
+
+gulp.task('webpack:test', function() {
+  return gulp.src('test/client/test_entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'test_bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('test/client/'));
 });
 
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);

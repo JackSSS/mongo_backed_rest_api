745ee7519ac4b9cfe9b4eaff1ee8c501bd7a21c9
Jack Sneed <sneed.jack@gmail.com>
2015-12-06 16:18:49 -0800

    Post test working

--- jack_sneed/test/client/ninjas_controller_tests.js
+++ jack_sneed/test/client/ninjas_controller_tests.js
@@ -19,4 +19,33 @@ describe('ninja controller', function() {
     expect(typeof controller).toBe('object');
     expect(Array.isArray($scope.ninjas)).toBe(true);
   });
+
+  describe('REST request functions', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $ControllerConstructor('NinjasController', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should add an array to ninjas with a GET all', function() {
+      $httpBackend.expectGET('/api/ninja').respond(200, [{_id: 1, name: 'test ninja'}]);
+      $scope.getAll();
+      $httpBackend.flush();
+      expect($scope.ninjas[0].name).toBe('test ninja');
+    });
+
+    it('should not be able to create a new ninja', function() {
+      $httpBackend.expectPOST('/api/ninja', {name: 'test ninja'}).respond(200, {name: 'a different ninja'});
+      expect($scope.ninjas.length).toBe(0);
+      $scope.newNinja = {name: 'test ninja'};
+      $scope.create($scope.newNinja);
+      $httpBackend.flush();
+      expect($scope.newNinja).toBeNull();
+    });
+  });
 });

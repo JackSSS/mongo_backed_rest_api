b7ab0588a8a446fc3bcc2185590de7bfc052d42f
Jack Sneed <sneed.jack@gmail.com>
2015-12-08 12:25:35 -0800

    add styles

--- jack_sneed/gulpfile.js
+++ jack_sneed/gulpfile.js
@@ -1,32 +1,27 @@
-'use strict';
-
-
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var minifyCss = require('gulp-minify-css');
 var concatCss = require('gulp-concat-css');
 var gulpWatch = require('gulp-watch');
-var sass = require('gulp-sass');
-var maps = require('gulp-sourcemaps');
-
 
 gulp.task('static:dev', function() {
   gulp.src('app/**/*.html')
   .pipe(gulp.dest('build/'));
 });
 
-
-gulp.task('sass:dev', function() {
-  gulp.src('./app/sass/**/*.scss')
-  .pipe(maps.init())
-  .pipe(sass().on('error', sass.logError))
+gulp.task('css:dev', function() {
+  return gulp.src([
+    'app/css/reset.css',
+    'app/css/base.css',
+    'app/css/layout.css',
+    'app/css/module.css'])
+  .pipe(concatCss('styles.min.css'))
   .pipe(minifyCss())
-  .pipe(maps.write('./'))
   .pipe(gulp.dest('build/'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch(['./app/sass/**/*.scss', './app/index.html'], ['sass:dev', 'static:dev']);
+gulp.task('css:watch', function () {
+  gulp.watch('./app/css/**/*.css', ['css:dev']);
 });
 
 gulp.task('webpack:dev', function() {
@@ -37,16 +32,6 @@ gulp.task('webpack:dev', function() {
     }
   }))
   .pipe(gulp.dest('build/'));
-});
-
-gulp.task('webpack:test', function() {
-  return gulp.src('test/client/test_entry.js')
-  .pipe(webpack({
-    output: {
-      filename: 'test_bundle.js'
-    }
-  }))
-  .pipe(gulp.dest('test/client/'));
 });
 
 gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
